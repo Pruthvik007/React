6cb8fff733c4c8be933a0a963f8e190ba277aab8
Sai Pruthvik <saipruthvik@mail.com>
2023-03-27 17:29:59 +0530

    changed homepage url again

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -2,19 +2,19 @@ import "./App.css";
 import List from "./task-list-app/components/List";
 import Task from "./task-list-app/components/Task";
 import Home from "./Home";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CountDown from "./CountDown";
 function App() {
   return (
     <div className="App">
-      <HashRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/countdown" element={<CountDown />}></Route>
           <Route path="/tasksList" element={<List />}></Route>
           <Route path="/task/:isEditMode" element={<Task />}></Route>
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
